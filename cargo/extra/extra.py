d1f4b4ae0c63c432597b9bd0396dfb4aba7ff778
shipperizer <alexcabb@gmail.com>
2015-07-12 19:35:39 +0100

    passing docker api config into methodview.as_view

--- cargo/extra/extra.py
+++ cargo/extra/extra.py
@@ -6,23 +6,24 @@ from urllib.parse import urljoin
 from flask.views import MethodView
 from flask import render_template, jsonify, request
 
-import cargo
 
 class ExtraAPI(MethodView):
 
     methods = ['GET']
 
+    def __init__(self, docker_api=None):
+        self.DOCKER_API = docker_api
+
     def get(self):
         json = request.args.get('json')
         version = self.version()
         info = self.info()
-        if not json in ['True', 'true']:
+        if json not in ['True', 'true']:
             return render_template('extra.html', version=version, info=info)
         return jsonify(version=version, info=info)
 
     def version(self):
-        return requests.get(urljoin(cargo.DOCKER_API, 'version')).json()
+        return requests.get(urljoin(self.DOCKER_API, 'version')).json()
 
     def info(self):
-        return requests.get(urljoin(cargo.DOCKER_API, 'info')).json()
-        
+        return requests.get(urljoin(self.DOCKER_API, 'info')).json()
